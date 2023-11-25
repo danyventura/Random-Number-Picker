759927019edf6202494b19112ade205a131c673d
danyventura <danyventura@hotmail.com>
2023-11-24 17:35:27 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+//Generate a random number
+// const randomNumber = Math.floor(1 + Math.random() * 5000);
+
+// Get the HTML element by its id
+// const randomNumberDisplay = document.getElementById('randomNumberDisplay');
+
+// Set the content of the element the the random number
+// randomNumberDisplay.textContent = `Random Number: ${randomNumber}`;
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 100;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
+}
+
+const list = [];
+for (let i = 1; i <= 500; i++) {
+  list.push(i);
+}
+
+// Set the value to the list
+textarea.value = list.join(',');
